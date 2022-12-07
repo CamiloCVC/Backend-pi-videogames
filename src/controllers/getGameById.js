5b4e87d12610da3a63542f6106b272589dd913f4
CamiloCVC <camicastellanos0602@gmail.com>
2022-12-07 12:25:56 -0500

    ya casi

--- src/controllers/getGameById.js
+++ src/controllers/getGameById.js
@@ -5,7 +5,7 @@ const { Videogame, Genre } = require('../db.js');
 const getApiGameById = async (id) =>{
     let gameById = []
     try {
-        let ApiGamesById = await axios.get(`https://api.rawg.io/api/games/${id}?key=fe0415ea52474be795146cbef5787185`);
+        let ApiGamesById = await axios.get(`https://api.rawg.io/api/games/${id}?key=fe0415ea52474be795146cbef5787185`)
         gameById.push({
             id: ApiGamesById.data.id,
             name: ApiGamesById.data.name,
